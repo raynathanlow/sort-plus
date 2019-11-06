67743f9261fe12700c5681d69495f42974cd147f
Ray Nathan Low <raynathanlow@gmail.com>
2019-11-05 20:22:12 -0800

    Adjust how DB is connected
    Change how the database is connected based on the Mongoose's Getting
    Started page.

--- server.js
+++ server.js
@@ -8,17 +8,20 @@ const app = express();
 const db = config.get('mongoURI');
 
 // Connect to MongoDB
-mongoose
-  .connect(db, { 
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  }) // Adding new mongo url parser
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
+mongoose.connect(db, { 
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
 
 // Bind default connection to error event (to get notification of connection errors)
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+connection.once('open', function() {
+  console.log("MongoDB connected...");
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
