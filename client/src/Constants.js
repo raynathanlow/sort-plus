0b8d307b4c3e3a016b4ac8be384eca2bbd453ba1
Ray Nathan Low <raynathanlow@gmail.com>
2019-12-26 11:21:18 -0800

    Use Heroku redirect URI

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,4 +1,4 @@
 export const STATE_KEY = "spotify_state";
 export const CLIENT_ID = "64ed00a411a84e818c3b591feb467f4b";
-export const REDIRECT_URI = "http://localhost:3000/callback";
-// "https://murmuring-shelf-66339.herokuapp.com/callback";
+export const REDIRECT_URI =
+  "https://murmuring-shelf-66339.herokuapp.com/callback";
