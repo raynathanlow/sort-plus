f4620c243d6495d7c13bd4e1a187789fcc23fd1d
Ray Nathan Low <raynathanlow@gmail.com>
2020-01-02 19:15:00 -0800

    Add missing protocol to target origin URL

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -2,4 +2,4 @@ export const STATE_KEY = "spotify_state";
 export const CLIENT_ID = "64ed00a411a84e818c3b591feb467f4b";
 export const REDIRECT_URI =
   "https://murmuring-shelf-66339.herokuapp.com/callback";
-export const TARGET_ORIGIN = "murmuring-shelf-66339.herokuapp.com/";
+export const TARGET_ORIGIN = "https://murmuring-shelf-66339.herokuapp.com/";
