88325039455daa604ec7a27a0897caf5f9fc127c
Ray Nathan Low <raynathanlow@gmail.com>
2020-01-13 15:49:40 -0800

    Replace service worker with a custom one
    Build script has been modified to build a custom Workbox and service worker.

---
+++ client/src/sw.js
@@ -0,0 +1,41 @@
+if ("function" === typeof importScripts) {
+  importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+  );
+  /* global workbox */
+  if (workbox) {
+    console.log("Workbox is loaded");
+
+    /* injection point for manifest files.  */
+    workbox.precaching.precacheAndRoute([]);
+
+    /* custom cache rules*/
+    workbox.routing.registerNavigationRoute("/index.html", {
+      blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
+    });
+
+    workbox.routing.registerRoute(
+      /\.(?:png|gif|jpg|jpeg)$/,
+      new workbox.strategies.CacheFirst({
+        cacheName: "images"
+      })
+    );
+
+    workbox.routing.registerRoute(
+      /api\/library.+/,
+      new workbox.strategies.CacheFirst({
+        cacheName: "data"
+      })
+    );
+
+    // Not sure if this is necessary? Might be redundant...
+    // workbox.routing.registerRoute(
+    //   /https:\/\/i.scdn.co\/image\/.+/,
+    //   new workbox.strategies.CacheFirst({
+    //     cacheName: "data"
+    //   })
+    // );
+  } else {
+    console.log("Workbox could not be loaded. No Offline support");
+  }
+}
